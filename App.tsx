248a2a27bbad45829af94c4985673d619bf71b87
André Barriga <a.f.c.barriga@gmail.com>
2022-01-18 19:05:18 +0000

    Init

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Bola</Text>
       <StatusBar style="auto" />
     </View>
   );
